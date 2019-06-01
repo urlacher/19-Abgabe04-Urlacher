dfaabb1af25e7ab875935554f06b4b51a42de49c
Thomas Urlacher <thomas.urlacher@edu.campus02.at>
2019-06-01 17:28:26 +0200

    3. JavaDoc implementation / TODO-List / Lab-Report

--- src/main/java/at/fhj/iit/Queue.java
+++ src/main/java/at/fhj/iit/Queue.java
@@ -1,19 +1,25 @@
 package at.fhj.iit;
 
+
+/**
+ * Interface for a queue. A Queue is a Data Structure working on the principals of FIFO (First in First out)
+ * @author Thomas Urlacher
+ * @version 1.0
+ */
 public interface Queue {
 	
 	/**
 	 *  Adds the element obj to the queue. 
 	 *  If the addition is successful, the method returns true else false.
-	 * @param obj
-	 * @return
+	 * @param obj Add new String to queue.
+	 * @return Boolean
 	 */
 	public abstract boolean offer(String obj);
 
 	/**
 	 *  Returns the head (first) element and also deletes it. That is, we cannot get it again. 
 	 *  If no element exists (when queue is empty), the method returns null.
-	 * @return
+	 * @return String
 	 */
 	public abstract String poll();
 	
@@ -21,21 +27,21 @@ public interface Queue {
 	 * It also returns and deletes the head element like poll(), but with a small difference. 
 	 * This method throws NoSuchElementException if the queue is empty. 
 	 * 
-	 * @return
+	 * @return String
 	 */
 	public abstract String remove();
 	
 	/**
 	 *  Returns the head element but it does not delete it. That is, we can get it again. 
 	 *  Returns null when the queue is empty. 
-	 * @return
+	 * @return String
 	 */
 	public abstract String peek();
 	
 	/**
 	 *  It works similar to peek() but with a small difference (returns but does not delete the element). 
 	 *  It throws NoSuchElementException when the queue is empty.
-	 * @return
+	 * @return String
 	 */
 	public abstract String element();
 	
--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -4,23 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-// there's some Bugs included, try to debug the code and fix the Bugs
-// there are different Bugs, wrong implementation, typos, ...
-// write Test-Cases (read Queue Interface for understanding methods) and use Debugging possibilies of your IDE
-
+/**
+ * StingQueue implementation of the {@link Queue} Interface.
+ *
+ * A Queue is a Data structure working on the FIFO (First in Fist Out) principal. This Queue is based on a
+ * {@link java.util.ArrayList ArrayList}.
+ *
+ * @author Thomas Urlacher
+ * @version 1.0
+ *
+ */
 public class StringQueue implements Queue {
 	
 	private List<String> elements = new ArrayList<>();
 	private int maxSize = 5;
 
+	/**
+	 * Default Constructor. Maximum of 5 Strings could be stored in this Queue
+	 */
 	public StringQueue(){
 
 	}
 
+	/**
+	 *  Constructor sets maximum Size of Queue.
+	 * @param maxSize Maximum Amount of String entries
+	 */
 	public StringQueue(int maxSize){
 		this.maxSize = maxSize;
 	}
 
+	/**
+	 * Inserts a new queue element.
+	 * @param obj Requires a new String
+	 * @return True if queue size smaller than maximum
+	 */
 	@Override
 	public boolean offer(String obj) {
 		if(elements.size()!= maxSize)
@@ -31,6 +49,10 @@ public class StringQueue implements Queue {
 		return true;
 	}
 
+	/**
+	 * Return and Remove next queue element.
+	 * @return String Value of the next Queue Item.
+	 */
 	@Override
 	public String poll() {
 		String element = peek();
@@ -42,6 +64,11 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
+	/**
+	 * Return and Remove next queue element.
+	 * @throws NoSuchElementException If there is no element in the queue
+	 * @return String Value of the next Queue Item.
+	 */
 	@Override
 	public String remove() {
 		String element = poll();		
@@ -51,6 +78,10 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
+	/**
+	 * Examine the next queue element. (Does not remove Entry)
+	 * @return String Value of the next Queue Item.
+	 */
 	@Override
 	public String peek() {
 		String element;
@@ -62,6 +93,11 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
+	/**
+	 * Examine the next queue element. (Does not remove Entry)
+	 * @throws NoSuchElementException If there is no element in the queue
+	 * @return String Value of the next Queue Item.
+	 */
 	@Override
 	public String element() {
 		String element = peek();
